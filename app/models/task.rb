8105fd91889a88ff7c61e0b7a676f71fe02e7358
IVAN <ivan061788@gmail.com>
2015-08-05 05:15:27 -0700

    Testing

--- app/models/task.rb
+++ app/models/task.rb
@@ -52,12 +52,12 @@ class Task < ActiveRecord::Base
     
     intercom = Intercom::Client.new(app_id: IntercomRails.config.app_id, api_key: IntercomRails.config.api_key)
 
-    intercom.events.create({
-      :event_name => "Task purchase", 
-      :email => self.buyer.email, 
-      :created_at => self.created_at.to_i,
-      :metadata => meta_info
-    })
+    # intercom.events.create({
+    #   :event_name => "Task purchase", 
+    #   :email => self.buyer.email, 
+    #   :created_at => self.created_at.to_i,
+    #   :metadata => meta_info
+    # })
   end
 
   def self.tasks_lapse
